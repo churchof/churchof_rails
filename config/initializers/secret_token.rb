4c56f98b7a75645d9da738043ac513915bde7377
Zach Whelchel <zach.whelchel@gmail.com>
2014-01-18 12:04:58 -0500

    Added project files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Churchof::Application.config.secret_key_base = '97f739c18732f3dbec359a1c0c0036eacdd797cbd8cb3df4e172ae51173f4e89251e61f03f216df7748e3ac26171ec6a106b83580669e6d5597b8df0f6f1fef1'
