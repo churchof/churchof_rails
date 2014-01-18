18c0414fb48b82fe5ccd71769bb20ac395ff4a10
Zach Whelchel <zach.whelchel@gmail.com>
2014-01-18 17:35:53 -0500

    Fixed issue where first_name and last_name weren't saving on the User model by reversing this tutorial:http://blog.12spokes.com/web-design-development/adding-custom-fields-to-your-devise-user-model-in-rails-4/ and instead following this one:https://gist.github.com/PicoReclame/7194282

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  protected
+  # Added to make Devise work with Rails 4.
+  before_filter :configure_devise_params, if: :devise_controller?
 
-  def devise_parameter_sanitizer
-    if resource_class == User
-      User::ParameterSanitizer.new(User, :user, params)
-    else
-      super # Use the default one
+  private
+
+  def configure_devise_params
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:first_name, :last_name, :name, :avatar, :avatar_cache, :email, :password, :password_confirmation)
+    end
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit(:first_name, :last_name, :name, :avatar,  :remove_avatar, :avatar_cache, :email, :password, :password_confirmation, :current_password)
     end
   end
-
 end
