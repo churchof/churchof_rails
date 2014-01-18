ff8fad452f951cc5a6aec68e9247e023a3c79603
Zach Whelchel <zach.whelchel@gmail.com>
2014-01-18 18:14:02 -0500

    Completed the has many relationship between needs and users and updated the views to show the user's name.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :needs
+         
+  def full_name
+  	first_name + " " + last_name
+  end
 end
