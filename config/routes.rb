dea8bae813c4b5e7533826762d734d1a3f61e03c
Zach Whelchel <zach.whelchel@gmail.com>
2014-01-18 13:59:03 -0500

    Installed Devise for authentication.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Churchof::Application.routes.draw do
   resources :needs
 
+  root :to => 'needs#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
