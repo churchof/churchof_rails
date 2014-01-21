e11f3b291f6f0872ce2aed892c3651d5d021c6f0
Zach Whelchel <zach.whelchel@gmail.com>
2014-01-20 21:44:39 -0500

    Added custom routes for login, register, and feed.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Churchof::Application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    get 'register', to: 'devise/registrations#new', as: :register
+    get 'login', to: 'devise/sessions#new', as: :login
+  end
+
   resources :needs
+
+  get 'feed', to: 'needs#index', as: :feed
 
   root :to => 'needs#index'
 
