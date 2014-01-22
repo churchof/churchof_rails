3bda4a333d1693dc051e39c27abe9deacdfae414
Zach Whelchel <zach.whelchel@gmail.com>
2014-01-22 13:50:38 -0500

    Added tests and validations for a need's title, description, and user_id.

--- app/models/need.rb
+++ app/models/need.rb
@@ -1,3 +1,7 @@
 class Need < ActiveRecord::Base
 	belongs_to :user
+
+	validates :description, presence: true, length: { minimum: 2 }
+	validates :title, presence: true
+	validates :user_id, presence: true
 end
--- test/models/need_test.rb
+++ test/models/need_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class NeedTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	test "that a need requires a title" do	
+		need = Need.new
+		assert !need.save
+		assert !need.errors[:title].empty?
+	end
+
+	test "that a need requires a description" do	
+		need = Need.new
+		assert !need.save
+		assert !need.errors[:description].empty?
+	end
+
+	test "that a need's description is at least 2 letters long" do	
+		need = Need.new
+		need.description = "a"
+		assert !need.save
+		assert !need.errors[:description].empty?
+	end
+
+	test "that a need has a user id" do	
+		need = Need.new
+		need.description = "abc"
+		assert !need.save
+		assert !need.errors[:user_id].empty?
+	end
 end
