b642022a1b72c2a30dfbf1048f3f6ef2c4b06749
Zach Whelchel <zach.whelchel@gmail.com>
2014-01-23 21:22:14 -0500

    Removed the defaulting of roles_mask to 1 in the migrate files in an attempt to get the site working on Heroku.

--- db/migrate/20140122215321_add_roles_mask_to_user.rb
+++ db/migrate/20140122215321_add_roles_mask_to_user.rb
@@ -1,9 +1,5 @@
 class AddRolesMaskToUser < ActiveRecord::Migration
   def change
-    add_column :users, :roles_mask, :integer, :default => 1
-    
-    User.find_each do |u|
-      u.update_column(:roles_mask, 1)
-    end
+    add_column :users, :roles_mask, :integer
   end
 end
