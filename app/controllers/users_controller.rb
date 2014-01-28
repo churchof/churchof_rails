b3582c19441dc97f4f3fca3f7f924649e1d46464
Zach Whelchel <zach.whelchel@gmail.com>
2014-01-27 20:54:01 -0500

    Added two methods that might be a good approach to getting roles set from the site? Not sure about them though.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,17 @@ class UsersController < ApplicationController
   def index
   	@users = User.all
   end
+
+  # Maybe this is the right way? It seems I need to do an actual POST to a url with params to do this the right way.
+  def assign_poster_role
+	@user = User.find(params[:id])
+	@user.add_role :poster
+	redirect_to @user
+  end
+
+  def remove_poster_role
+  	@user = User.find(params[:id])
+  	@user.remove_role :poster
+  	redirect_to @user
+  end
 end
