f5d26dfa78013fc2fb0292a1e6ed24c5e2a4a7ed
Zach Whelchel <zach.whelchel@gmail.com>
2014-02-01 22:49:10 -0500

    The User Panel now shows the appropriate needs for each section.

--- app/controllers/need_poster_panel_controller.rb
+++ app/controllers/need_poster_panel_controller.rb
@@ -1,4 +1,7 @@
 class NeedPosterPanelController < ApplicationController
   def index
+  	@needs_admin_incoming = Need.where(user_posted_by: current_user, need_stage: 1)
+  	@needs_admin_in_progress = Need.where(user_posted_by: current_user, need_stage: 2)
+  	@needs_admin_completed = Need.where(user_posted_by: current_user, need_stage: 3)
   end
 end
