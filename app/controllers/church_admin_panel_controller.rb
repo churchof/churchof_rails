ffc4067a855e437067b6db6f203f5f65e0886191
Zach Whelchel <zach.whelchel@gmail.com>
2014-02-01 21:16:43 -0500

    Needs posted now are assaigned a Church Admin. The Church Admin panel accurately shows the needs assaigned to the Church Admin that is logged in.

--- app/controllers/church_admin_panel_controller.rb
+++ app/controllers/church_admin_panel_controller.rb
@@ -1,5 +1,7 @@
 class ChurchAdminPanelController < ApplicationController
   def index
-  	@needs = Need.all
+  	@needs_admin_incoming = Need.where(user_church_admin: current_user, need_stage: 1)
+  	@needs_admin_in_progress = Need.where(user_church_admin: current_user, need_stage: 2)
+  	@needs_admin_completed = Need.where(user_church_admin: current_user, need_stage: 3)
   end
 end
