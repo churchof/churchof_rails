1d9facaaf26d917fb95687fb43872322eeb294ca
melaniehall <melaniehalldesign@gmail.com>
2014-02-01 23:09:06 -0600

    UserIndex: SuperAdmin can update all user roles #3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,23 +3,18 @@ class UsersController < ApplicationController
   	@users = User.all
   end
 
-  # Maybe this is the right way? It seems I need to do an actual POST to a url with params to do this the right way.
-  def assign_need_poster_role
-	@user = User.find(params[:id])
-	@user.add_role :need_poster
-	redirect_to @user
-  end
-
-  def remove_need_poster_role
-  	@user = User.find(params[:id])
-  	@user.remove_role :need_poster
-  	redirect_to @user
-  end
-
-  # Added... but I don't want to do the template also to make this work... and is this even
-  # ok with Devise? http://stackoverflow.com/questions/5212350/making-changes-to-the-devise-user-controller-in-rails
   def update
-   #edit here
+   @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to :back, notice: 'User was successfully updated.'
+    else
+      render action: 'index'
+      redirect_to :back, notice: 'User could not be updated.'
+    end
   end
 
+  private
+    def user_params
+      params.require(:user).permit!
+    end
 end
