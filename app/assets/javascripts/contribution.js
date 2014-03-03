5476cb2a884071d2d9c2333bcc43a9d2676a4512
Todd Willey <xtoddx@gmail.com>
2014-03-03 10:48:08 -0500

    Stripe form

---
+++ app/assets/javascripts/contribution.js
@@ -0,0 +1,25 @@
+  jQuery(function($) { Stripe.setPublishableKey('pk_test_EvYspVJmIJvMohKKhAykkgwA');})
+
+  var stripeResponseHandler = function(status, response) {
+    var $form = $('#new_contribution');
+    if (response.error) {
+      $form.find('.payment-errors').text(response.error.message);
+      $form.find('button').prop('disabled', false);
+    } else {
+      var token = response.id;
+      var currency = response.currency || 'USD';
+      $form.append($('<input type="hidden" name="contribution[stripe_token]" />').val(token));
+      $form.append($('<input type="hidden" name="contribution[stripe_currency]" />').val(currency));
+      // and re-submit
+      $form.get(0).submit();
+    }
+  };
+
+  jQuery(function($) {
+    $('#new_contribution').submit(function(e) {
+      var $form = $(this);
+      $form.find('button').prop('disabled', true);
+      Stripe.createToken($form, stripeResponseHandler);
+      return false;
+    });
+  });
