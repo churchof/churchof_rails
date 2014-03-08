2f86c5e15b677b0dabe71c9cbee987b65cbf93bd
Zach Whelchel <zach.whelchel@gmail.com>
2014-03-08 02:31:35 -0500

    Cleanup from git merge.

--- app/models/need.rb
+++ app/models/need.rb
@@ -5,19 +5,11 @@ class Need < ActiveRecord::Base
 	enumerize :need_stage, in: {:admin_incoming => 1, :admin_in_progress => 2, :admin_completed => 3}, default: :admin_incoming
 	enumerize :gender, in: {:unknown => 1, :male => 2, :female => 3}
 
-<<<<<<< HEAD
-  belongs_to :user_posted_by, :foreign_key => 'user_id_posted_by', :class_name => "User"
-  belongs_to :user_church_admin, :foreign_key => 'user_id_church_admin', :class_name => "User"
-  
-  has_many :contributions
-	has_many :expenses
-=======
   	belongs_to :user_posted_by, :foreign_key => 'user_id_posted_by', :class_name => "User"
   	belongs_to :user_church_admin, :foreign_key => 'user_id_church_admin', :class_name => "User"
   	
   	has_many :contributions
   	has_many :skills
->>>>>>> skills
 
 	validates :title, presence: true
 	validates :title_public, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20140228011745) do
-=======
 ActiveRecord::Schema.define(version: 20140308022249) do
->>>>>>> skills
 
   create_table "contributions", force: true do |t|
     t.integer  "contributor_id"
