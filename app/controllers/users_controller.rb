df7723ea1eb47aa7dfb940bec39f39a8c3048c2d
Zach Whelchel <zach.whelchel@gmail.com>
2014-03-19 16:13:34 -0400

    This gets us started on #64.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,10 @@ class UsersController < ApplicationController
   	@users = User.all
   end
 
+  def show
+   @user = User.find(params[:id])
+  end
+
   def update
    @user = User.find(params[:id])
     if @user.update(user_params)
