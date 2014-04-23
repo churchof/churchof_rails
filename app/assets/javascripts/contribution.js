040b912891a8dc3f9f1b4294baab88777d9cb17a
Zach Whelchel <zach.whelchel@gmail.com>
2014-04-23 13:57:53 -0400

    Added some alerts to diagnose heroku error.

--- app/assets/javascripts/contribution.js
+++ app/assets/javascripts/contribution.js
@@ -3,11 +3,17 @@
 jQuery(function($) { Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));})
 
   var stripeResponseHandler = function(status, response) {
+
+
     var $form = $('#new_contribution');
     if (response.error) {
+
+          alert("string response - error");
       $form.find('.payment-errors').text(response.error.message);
       $form.find('button').prop('disabled', false);
     } else {
+                alert("string response - good");
+
       var token = response.id;
       var currency = response.currency || 'USD';
       $form.append($('<input type="hidden" name="contribution[stripe_token]" />').val(token));
@@ -19,9 +25,7 @@ jQuery(function($) { Stripe.setPublishableKey($('meta[name="stripe-key"]').attr(
 
   jQuery(function($) {
     $('#new_contribution').submit(function(e) {
-
-alert("hello");
-
+      alert("submit");
       var $form = $(this);
       $form.find('button').prop('disabled', true);
       Stripe.createToken($form, stripeResponseHandler);
