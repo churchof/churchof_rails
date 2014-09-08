ede83027204615cb294954559dd322adf2cbf020
Zach Whelchel <zach.whelchel@gmail.com>
2014-09-08 19:51:30 -0400

    Removed old nonversioned api file.

--- app/controllers/api/resources_controller.rb
+++
@@ -1,9 +0,0 @@
-class API::ResourcesController < ApplicationController
-	skip_authorization_check
-  def index
-    @resources = Resource.first
-    respond_to do |format|
-      format.json { render :json => @resources }
-    end
-  end
-end
--- app/controllers/api/v1/resources_controller.rb
+++ app/controllers/api/v1/resources_controller.rb
@@ -1,3 +1,8 @@
 class API::V1::ResourcesController < ApplicationController
-
+  def index
+    @resources = Resource.all
+    respond_to do |format|
+      format.json { render :json => @resources }
+    end
+  end
 end
