9709e6fd606ec84ac29ca1efe4a6baeb319757ce
Zach Whelchel <zach.whelchel@gmail.com>
2014-09-08 20:23:03 -0400

    Fixed bug in API.

--- app/controllers/api/v1/resources_controller.rb
+++ app/controllers/api/v1/resources_controller.rb
@@ -1,5 +1,7 @@
 class API::V1::ResourcesController < ApplicationController
 	skip_authorization_check
+
+	# note the api should have another thing passed in that says the scope... currently index is doing something more specific.
   def index
     @resources = Resource.public.available.joins(:skills).where("skills.name ILIKE ?", "Free Meals").uniq
 
@@ -18,13 +20,13 @@ class API::V1::ResourcesController < ApplicationController
 	  list = value.map do |resource|
 	    { :id => "#{resource.id}",
 	      :title => resource.title.to_s,
-	      :description => resource.description.to_s
-	      :last_updated => resource.updated_at.to_s
-	      :contact_phone => resource.contact_phone.to_s
-	      :availability_status => resource.availability_status.to_s
-	      :public_status => resource.public_status.to_s
-	      :latitude => resource.latitude.to_s
-	      :longitude => resource.longitude.to_s
+	      :description => resource.description.to_s,
+	      :last_updated => resource.updated_at.to_s,
+	      :contact_phone => resource.contact_phone.to_s,
+	      :availability_status => resource.availability_status.to_s,
+	      :public_status => resource.public_status.to_s,
+	      :latitude => resource.latitude.to_s,
+	      :longitude => resource.longitude.to_s,
 	      :organization_title => resource.organization.title.to_s
 	    }
 		end
