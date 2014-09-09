0665c8b474cd26a40b385fda5806ae31d376dab4
Zach Whelchel <zach.whelchel@gmail.com>
2014-09-08 20:03:03 -0400

    Changed api output to use custom json writing.

--- app/controllers/api/v1/resources_controller.rb
+++ app/controllers/api/v1/resources_controller.rb
@@ -3,7 +3,23 @@ class API::V1::ResourcesController < ApplicationController
   def index
     @resources = Resource.all
     respond_to do |format|
-      format.json { render :json => @resources }
+      format.json do	
+			  render :json => custom_json_for(@resources)
+			end
     end
   end
+
+
+  private
+
+	def custom_json_for(value)
+	  list = value.map do |resource|
+	    { :id => " #{resource.id}",
+	      :title => resource.title.to_s,
+	      :description => resource.description.to_s
+	    }
+		end
+		list.to_json
+	end
+
 end
