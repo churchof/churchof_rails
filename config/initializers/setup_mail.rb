04a5f099d62eebf9a4af2538277b0308b2bf4adb
Zach Whelchel <zach.whelchel@gmail.com>
2014-10-07 23:04:48 -0400

    Another zoho attempt.

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,8 +1,9 @@
 ActionMailer::Base.delivery_method = :smtp  
 ActionMailer::Base.smtp_settings = {            
   :address              => "smtp.zoho.com", 
-  :port                 => 465,                 
-  :user_name            => 'no-reply@church-of.com',
+  :port                 => 465,   
+  :domain 				=> "zoho.com",
+  :user_name            => "no-reply@church-of.com",
   :password             => ENV['ZOHO_PASSWORD'],         
   :authentication       => :plain,
   :ssl                  => true,
