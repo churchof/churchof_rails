694f4cf3e9f1b6b81d910775c389099de4c7d9f7
Zach Whelchel <zach.whelchel@gmail.com>
2014-10-08 00:43:54 -0400

    Maybe this will fix zoho?

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -7,7 +7,7 @@ class Mailer < ActionMailer::Base
 # System
 
 def activity_was_not_created(description)
-	mail :to => "zach.whelchel@gmail.com", :from => "churchoflexington@gmail.com", :subject => description
+	mail :to => "zach.whelchel@gmail.com", :from => "no-reply@church-of.com", :subject => description
 end
 
 # Church Admins
@@ -16,20 +16,20 @@ def church_admin_new_need_admin_incoming(need_id, user_posted_by_id, user_church
 	@need = Need.find(need_id)
 	@user_posted_by = User.find(user_posted_by_id)
 	@user_church_admin = User.find(user_church_admin_id)
-	mail :to => @user_church_admin.email, :from => "churchoflexington@gmail.com", :subject => "Church Admin Incoming Need: #{@need.title}"
+	mail :to => @user_church_admin.email, :from => "no-reply@church-of.com", :subject => "Church Admin Incoming Need: #{@need.title}"
 end
 
 def church_admin_need_recieved_contribution(user_church_admin_id, need_id, contribution_id)
 	@user_church_admin = User.find(user_church_admin_id)
 	@need = Need.find(need_id)
 	@contribution = Contribution.find(contribution_id)
-	mail :to => @user_church_admin.email, :from => "churchoflexington@gmail.com", :subject => "Need (You Admin) Recieved Contribution: #{@need.title}, #{@contribution.amount}"
+	mail :to => @user_church_admin.email, :from => "no-reply@church-of.com", :subject => "Need (You Admin) Recieved Contribution: #{@need.title}, #{@contribution.amount}"
 end
 
 def church_admin_need_fully_funded(user_church_admin_id, need_id)
 	@user_church_admin = User.find(user_church_admin_id)
 	@need = Need.find(need_id)
-	mail :to => @user_church_admin.email, :from => "churchoflexington@gmail.com", :subject => "Need (You Admin) Is Fully Funded: #{@need.title}"
+	mail :to => @user_church_admin.email, :from => "no-reply@church-of.com", :subject => "Need (You Admin) Is Fully Funded: #{@need.title}"
 end
 
 # Need Posters
@@ -38,27 +38,27 @@ def user_posted_by_need_moved_to_in_progress(user_posted_by_id, need_id, user_ch
 	@user_posted_by = User.find(user_posted_by_id)
 	@need = Need.find(need_id)
 	@user_church_admin = User.find(user_church_admin_id)
-	mail :to => @user_posted_by.email, :from => "churchoflexington@gmail.com", :subject => "Need (You Posted) Moved to In Progress: #{@need.title}"
+	mail :to => @user_posted_by.email, :from => "no-reply@church-of.com", :subject => "Need (You Posted) Moved to In Progress: #{@need.title}"
 end
 
 def user_posted_by_need_recieved_contribution(user_posted_by_id, need_id, contribution_id)
 	@user_posted_by = User.find(user_posted_by_id)
 	@need = Need.find(need_id)
 	@contribution = Contribution.find(contribution_id)
-	mail :to => @user_posted_by.email, :from => "churchoflexington@gmail.com", :subject => "Need (You Posted) Recieved Contribution: #{@need.title}, #{@contribution.amount}"
+	mail :to => @user_posted_by.email, :from => "no-reply@church-of.com", :subject => "Need (You Posted) Recieved Contribution: #{@need.title}, #{@contribution.amount}"
 end
 
 def user_posted_by_need_fully_funded(user_posted_by_id, need_id)
 	@user_posted_by = User.find(user_posted_by_id)
 	@need = Need.find(need_id)
-	mail :to => @user_posted_by.email, :from => "churchoflexington@gmail.com", :subject => "Need (You Posted) Is Fully Funded: #{@need.title}"
+	mail :to => @user_posted_by.email, :from => "no-reply@church-of.com", :subject => "Need (You Posted) Is Fully Funded: #{@need.title}"
 end
 
 def user_posted_by_public_update_added(user_posted_by_id, need_id, update_id)
 	@user_posted_by = User.find(user_posted_by_id)
 	@need = Need.find(need_id)
 	@update = Update.find(update_id)
-	mail :to => @user_posted_by.email, :from => "churchoflexington@gmail.com", :subject => "Need (You Posted) Has a New Public Update: #{@need.title}"
+	mail :to => @user_posted_by.email, :from => "no-reply@church-of.com", :subject => "Need (You Posted) Has a New Public Update: #{@need.title}"
 end
 
 # Users
@@ -67,33 +67,33 @@ def user_new_need_with_matching_skills(user_id, need_id, skill_ids)
 	@user = User.find(user_id)
 	@need = Need.find(need_id)
 	@skills = Skill.find(skill_ids)
-	mail :to => @user.email, :from => "churchoflexington@gmail.com", :subject => "New Need Matching Skills: #{@need.title_public}"
+	mail :to => @user.email, :from => "no-reply@church-of.com", :subject => "New Need Matching Skills: #{@need.title_public}"
 end
 
 def user_need_contributed_to_public_update_added(user_id, need_id, update_id)
 	@user = User.find(user_id)
 	@need = Need.find(need_id)
 	@update = Update.find(update_id)
-	mail :to => @user.email, :from => "churchoflexington@gmail.com", :subject => "Need (You Contributed To) Has a New Public Update: #{@need.title_public}"
+	mail :to => @user.email, :from => "no-reply@church-of.com", :subject => "Need (You Contributed To) Has a New Public Update: #{@need.title_public}"
 end
 
 def user_need_contributed_to_fully_funded(user_id, need_id)
 	@user = User.find(user_id)
 	@need = Need.find(need_id)
-	mail :to => @user.email, :from => "churchoflexington@gmail.com", :subject => "Need (You Contributed To) Is Fully Funded: #{@need.title_public}"
+	mail :to => @user.email, :from => "no-reply@church-of.com", :subject => "Need (You Contributed To) Is Fully Funded: #{@need.title_public}"
 end
 
 def receipt_to_user(user_id, need_id, contribution_id)
 	@user = User.find(user_id)
 	@need = Need.find(need_id)
 	@contribution = Contribution.find(contribution_id)
-	mail :to => @user.email, :from => "churchoflexington@gmail.com", :subject => "Thanks for contributing to: #{@need.title_public}"
+	mail :to => @user.email, :from => "no-reply@church-of.com", :subject => "Thanks for contributing to: #{@need.title_public}"
 end
 
 def user_need_contributed_to_fully_volunteered(user_id, need_id)
 	@user = User.find(user_id)
 	@need = Need.find(need_id)
-	mail :to => @user.email, :from => "churchoflexington@gmail.com", :subject => "Need (You Contributed To) is Fully Volunteered: #{@need.title_public}"
+	mail :to => @user.email, :from => "no-reply@church-of.com", :subject => "Need (You Contributed To) is Fully Volunteered: #{@need.title_public}"
 end
 
 # Contributors
@@ -102,26 +102,26 @@ def contributor_need_contributed_to_public_update_added(contributor_id, need_id,
 	@contributor = Contributor.find(contributor_id)
 	@need = Need.find(need_id)
 	@update = Update.find(update_id)
-	mail :to => @contributor.email, :from => "churchoflexington@gmail.com", :subject => "Need (You Contributed To) Has a New Public Update: #{@need.title_public}"
+	mail :to => @contributor.email, :from => "no-reply@church-of.com", :subject => "Need (You Contributed To) Has a New Public Update: #{@need.title_public}"
 end
 
 def contributor_need_contributed_to_fully_funded(contributor_id, need_id)
 	@contributor = Contributor.find(contributor_id)
 	@need = Need.find(need_id)
-	mail :to => @contributor.email, :from => "churchoflexington@gmail.com", :subject => "Need (You Contributed To) Is Fully Funded: #{@need.title_public}"
+	mail :to => @contributor.email, :from => "no-reply@church-of.com", :subject => "Need (You Contributed To) Is Fully Funded: #{@need.title_public}"
 end
 
 def receipt_to_contributor(contributor_id, need_id, contribution_id)
 	@contributor = Contributor.find(contributor_id)
 	@need = Need.find(need_id)
 	@contribution = Contribution.find(contribution_id)
-	mail :to => @contributor.email, :from => "churchoflexington@gmail.com", :subject => "Thanks for contributing to: #{@need.title_public}"
+	mail :to => @contributor.email, :from => "no-reply@church-of.com", :subject => "Thanks for contributing to: #{@need.title_public}"
 end
 
 def contributor_need_contributed_to_fully_volunteered(contributor_id, need_id)
 	@contributor = Contributor.find(contributor_id)
 	@need = Need.find(need_id)
-	mail :to => @contributor.email, :from => "churchoflexington@gmail.com", :subject => "Need (You Contributed To) is Fully Volunteered: #{@need.title_public}"
+	mail :to => @contributor.email, :from => "no-reply@church-of.com", :subject => "Need (You Contributed To) is Fully Volunteered: #{@need.title_public}"
 end
 
 # Need Leaders
@@ -130,47 +130,47 @@ def need_leader_new_need_assigned(need_leader_user_id, church_admin_user_id, nee
 	@need_leader = User.find(need_leader_user_id)
 	@church_admin = User.find(church_admin_user_id)
 	@need = Need.find(need_id)
-	mail :to => @need_leader.email, :from => "churchoflexington@gmail.com", :subject => "Leading New Need: #{@need.title_public}"
+	mail :to => @need_leader.email, :from => "no-reply@church-of.com", :subject => "Leading New Need: #{@need.title_public}"
 end
 
 def need_leader_new_volunteer_added(need_leader_user_id, volunteer_user_id, need_id)
 	@need_leader = User.find(need_leader_user_id)
 	@volunteer = User.find(volunteer_user_id)
 	@need = Need.find(need_id)
-	mail :to => @need_leader.email, :from => "churchoflexington@gmail.com", :subject => "Need (You Lead) Has a New Volunteer: #{@volunteer.full_name}"
+	mail :to => @need_leader.email, :from => "no-reply@church-of.com", :subject => "Need (You Lead) Has a New Volunteer: #{@volunteer.full_name}"
 end
 
 def need_leader_new_volunteer_status_changed_to_cancelled(need_leader_user_id, volunteer_user_id, need_id)
 	@need_leader = User.find(need_leader_user_id)
 	@volunteer = User.find(volunteer_user_id)
 	@need = Need.find(need_id)
-	mail :to => @need_leader.email, :from => "churchoflexington@gmail.com", :subject => "Need (You Lead) Has a Volunteer that Cancelled: #{@volunteer.full_name}"
+	mail :to => @need_leader.email, :from => "no-reply@church-of.com", :subject => "Need (You Lead) Has a Volunteer that Cancelled: #{@volunteer.full_name}"
 end
 
 def need_leader_new_volunteer_status_changed_to_renewed(need_leader_user_id, volunteer_user_id, need_id)
 	@need_leader = User.find(need_leader_user_id)
 	@volunteer = User.find(volunteer_user_id)
 	@need = Need.find(need_id)
-	mail :to => @need_leader.email, :from => "churchoflexington@gmail.com", :subject => "Need (You Lead) Has a Volunteer that Re-Signed Up: #{@volunteer.full_name}"
+	mail :to => @need_leader.email, :from => "no-reply@church-of.com", :subject => "Need (You Lead) Has a Volunteer that Re-Signed Up: #{@volunteer.full_name}"
 end
 
 def need_leader_need_fully_volunteered(need_leader_user_id, need_id)
 	@need_leader = User.find(need_leader_user_id)
 	@need = Need.find(need_id)
-	mail :to => @need_leader.email, :from => "churchoflexington@gmail.com", :subject => "Need (You Lead) is Fully Volunteered: #{@need.title_public}"
+	mail :to => @need_leader.email, :from => "no-reply@church-of.com", :subject => "Need (You Lead) is Fully Volunteered: #{@need.title_public}"
 end
 
 def need_leader_need_recieved_contribution(need_leader_user_id, need_id, contribution_id)
 	@need_leader = User.find(need_leader_user_id)
 	@need = Need.find(need_id)
 	@contribution = Contribution.find(contribution_id)
-	mail :to => @need_leader.email, :from => "churchoflexington@gmail.com", :subject => "Need (You Lead) Recieved Contribution: #{@need.title}, #{@contribution.amount}"
+	mail :to => @need_leader.email, :from => "no-reply@church-of.com", :subject => "Need (You Lead) Recieved Contribution: #{@need.title}, #{@contribution.amount}"
 end
 
 def need_leader_need_fully_funded(need_leader_user_id, need_id)
 	@need_leader = User.find(need_leader_user_id)
 	@need = Need.find(need_id)
-	mail :to => @need_leader.email, :from => "churchoflexington@gmail.com", :subject => "Need (You Lead) Is Fully Funded: #{@need.title}"
+	mail :to => @need_leader.email, :from => "no-reply@church-of.com", :subject => "Need (You Lead) Is Fully Funded: #{@need.title}"
 end
 
 # Volunteers
@@ -179,25 +179,25 @@ def volunteer_need_volunteered_for_public_update_added(user_id, need_id, update_
 	@user = User.find(user_id)
 	@need = Need.find(need_id)
 	@update = Update.find(update_id)
-	mail :to => @user.email, :from => "churchoflexington@gmail.com", :subject => "Need (You Signed Up to Volunteer for) Has a New Public Update: #{@need.title_public}"
+	mail :to => @user.email, :from => "no-reply@church-of.com", :subject => "Need (You Signed Up to Volunteer for) Has a New Public Update: #{@need.title_public}"
 end
 
 def volunteer_need_volunteered_for_fully_funded(user_id, need_id)
 	@user = User.find(user_id)
 	@need = Need.find(need_id)
-	mail :to => @user.email, :from => "churchoflexington@gmail.com", :subject => "Need (You Signed Up to Volunteer for) is Fully Funded: #{@need.title_public}"
+	mail :to => @user.email, :from => "no-reply@church-of.com", :subject => "Need (You Signed Up to Volunteer for) is Fully Funded: #{@need.title_public}"
 end
 
 def volunteer_need_volunteered_for_fully_volunteered(user_id, need_id)
 	@volunteer = User.find(user_id)
 	@need = Need.find(need_id)
-	mail :to => @volunteer.email, :from => "churchoflexington@gmail.com", :subject => "Need (You Signed Up to Volunteer for) is Fully Volunteered: #{@need.title_public}"
+	mail :to => @volunteer.email, :from => "no-reply@church-of.com", :subject => "Need (You Signed Up to Volunteer for) is Fully Volunteered: #{@need.title_public}"
 end
 
 def volunteer_instructions_to_volunteer(user_id, need_id)
 	@volunteer = User.find(user_id)
 	@need = Need.find(need_id)
-	mail :to => @volunteer.email, :from => "churchoflexington@gmail.com", :subject => "Volunteer Details for: #{@need.title_public}"
+	mail :to => @volunteer.email, :from => "no-reply@church-of.com", :subject => "Volunteer Details for: #{@need.title_public}"
 end
 
 end
