1c9eed803cac23db7c096a87589fd90898ee2eca
Zach Whelchel <zach.whelchel@gmail.com>
2014-10-07 22:42:07 -0400

    Changed email to zoho.

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,10 +1,11 @@
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-:address => "smtp.gmail.com",
-:port => 587,
-:domain => "gmail.com",
-:user_name => "no-reply@church-of.com",
-:password => ENV['GMAIL_PASSWORD'],
-:authentication => "plain",
-:enable_starttls_auto => true
+ActionMailer::Base.delivery_method = :smtp  
+ActionMailer::Base.smtp_settings = {            
+  :address              => "smtp.zoho.com", 
+  :port                 => 465,                 
+  :user_name            => 'no-reply@church-of.com',
+  :password             => ENV['ZOHO_PASSWORD'],         
+  :authentication       => :login,
+  :ssl                  => true,
+  :tls                  => true,
+  :enable_starttls_auto => true    
 }
